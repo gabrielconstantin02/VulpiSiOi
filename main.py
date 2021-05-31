379ca3f7cdb434ad2e6a1444080104264cbec889
Kira060200 <constantin.gab02@gmail.com>
2021-05-31 17:58:26 +0300

    implementare functie de verificare stare finala

--- main.py
+++ main.py
@@ -3,6 +3,119 @@ import sys
 import math
 
 
+def verifica_oi(listaOi):
+    """
+    functie care verifica daca 9 oi au ajuns in patratul de sus
+    :param listaOi:
+    :return:
+    """
+    nr = 0
+    for x in listaOi:
+        oaie = ((x[0] - Graph.translatie) // Graph.scalare, (x[1] - Graph.translatie) // Graph.scalare)
+        if oaie in Graph.noduri[:6] + Graph.noduri[8:11]:
+            nr += 1
+    if nr == 9:
+        return True
+    return False
+
+
+class Joc:
+    """
+    Clasa care defineste jocul. Se va schimba de la un joc la altul.
+    """
+    NR_COLOANE = 3
+    JMIN = None
+    JMAX = None
+    GOL = '#'
+
+    '''
+    @classmethod
+    def initializeaza(cls, display, NR_COLOANE=3, dim_celula=100):
+        cls.display = display
+        cls.dim_celula = dim_celula
+        cls.x_img = pygame.image.load('ics.png')
+        cls.x_img = pygame.transform.scale(cls.x_img, (dim_celula, dim_celula))
+        cls.zero_img = pygame.image.load('zero.png')
+        cls.zero_img = pygame.transform.scale(cls.zero_img, (dim_celula, dim_celula))
+        cls.celuleGrid = []  # este lista cu patratelele din grid
+        for linie in range(NR_COLOANE):
+            for coloana in range(NR_COLOANE):
+                patr = pygame.Rect(coloana * (dim_celula + 1), linie * (dim_celula + 1), dim_celula, dim_celula)
+                cls.celuleGrid.append(patr)
+
+    def deseneaza_grid(self, marcaj=None):  # tabla de exemplu este ["#","x","#","0",......]
+
+        for ind in range(len(self.matr)):
+            linie = ind // 3  # // inseamna div
+            coloana = ind % 3
+
+            if marcaj == ind:
+                # daca am o patratica selectata, o desenez cu rosu
+                culoare = (255, 0, 0)
+            else:
+                # altfel o desenez cu alb
+                culoare = (255, 255, 255)
+            pygame.draw.rect(self.__class__.display, culoare, self.__class__.celuleGrid[ind])  # alb = (255,255,255)
+            if self.matr[ind] == 'x':
+                self.__class__.display.blit(self.__class__.x_img, (
+                coloana * (self.__class__.dim_celula + 1), linie * (self.__class__.dim_celula + 1)))
+            elif self.matr[ind] == '0':
+                self.__class__.display.blit(self.__class__.zero_img, (
+                coloana * (self.__class__.dim_celula + 1), linie * (self.__class__.dim_celula + 1)))
+        pygame.display.flip()  # obligatoriu pentru a actualiza interfata (desenul)
+
+    # pygame.display.update()
+'''
+    def __init__(self):
+        self.noduri = Graph.noduri
+        self.muchii = Graph.muchii
+        self.pieseOi = [[x * Graph.scalare + Graph.translatie, y * Graph.scalare + Graph.translatie] for (x, y) in
+                   Graph.noduri[13:]]
+        self.pieseVulpi = [[220, 20], [420, 20]]
+
+
+    @classmethod
+    def jucator_opus(cls, jucator):
+        return cls.JMAX if jucator == cls.JMIN else cls.JMIN
+
+    def final(self):
+        rez = len(self.pieseOi) < 9 or verifica_oi(self.pieseOi)
+        if rez:
+            return rez
+        else:
+            return False
+
+    def mutari(self, jucator_opus):
+        l_mutari = []
+        for i in range(len(self.matr)):
+            if self.matr[i] == self.__class__.GOL:
+                matr_tabla_noua = list(self.matr)
+                matr_tabla_noua[i] = jucator_opus
+                l_mutari.append(Joc(matr_tabla_noua))
+        return l_mutari
+
+
+    def estimeaza_scor(self, adancime):
+        t_final = self.final()
+        # if (adancime==0):
+        if t_final == self.__class__.JMAX:
+            return (99 + adancime)
+        elif t_final == self.__class__.JMIN:
+            return (-99 - adancime)
+        elif t_final == 'remiza':
+            return 0
+        else:
+            return (self.linii_deschise(self.__class__.JMAX) - self.linii_deschise(self.__class__.JMIN))
+
+    def __str__(self):
+        sir = (" ".join([str(x) for x in self.matr[0:3]]) + "\n" +
+               " ".join([str(x) for x in self.matr[3:6]]) + "\n" +
+               " ".join([str(x) for x in self.matr[6:9]]) + "\n")
+
+        return sir
+
+
+
 def distEuclid(p0, p1):
     (x0, y0) = p0
     (x1, y1) = p1
@@ -78,9 +191,9 @@ piesaSelectata = pygame.image.load('piesa-neagra.png')
 piesaSelectata = pygame.transform.scale(piesaSelectata, (diametruPiesa, diametruPiesa))
 nodPiesaSelectata = False
 coordonateNoduri = [[Graph.translatie + Graph.scalare * x for x in nod] for nod in Graph.noduri]
-pieseOi = [[x * 100 + 20, y * 100 + 20] for (x, y) in Graph.noduri[13:]]
+# pieseOi = [[x * Graph.scalare + Graph.translatie, y * Graph.scalare + Graph.translatie] for (x, y) in Graph.noduri[13:]]
 nodPiesaSelectata = None
-pieseVulpi = [[220, 20], [420, 20]]
+# pieseVulpi = [[220, 20], [420, 20]]
 
 
 def deseneazaEcranJoc():
@@ -93,21 +206,24 @@ def deseneazaEcranJoc():
         p0 = coordonateNoduri[muchie[0]]
         p1 = coordonateNoduri[muchie[1]]
         pygame.draw.line(surface=ecran, color=culoareLinii, start_pos=p0, end_pos=p1, width=5)
-    for nod in pieseOi:
+    for nod in joc.pieseOi:
         ecran.blit(piesaOaie, (nod[0] - Graph.razaPiesa, nod[1] - Graph.razaPiesa))
-    for nod in pieseVulpi:
+    for nod in joc.pieseVulpi:
         ecran.blit(piesaVulpe, (nod[0] - Graph.razaPiesa, nod[1] - Graph.razaPiesa))
     if nodPiesaSelectata:
         ecran.blit(piesaSelectata, (nodPiesaSelectata[0] - Graph.razaPiesa, nodPiesaSelectata[1] - Graph.razaPiesa))
     pygame.display.update()
 
 
+joc = Joc()
 deseneazaEcranJoc()
 rand = 0
+# print(verifica_oi([ [320, 20], [220, 120], [220, 20], [420, 20], [320, 120], [220, 220], [420, 120], [420, 220], [320, 220]]))
 
 print("Muta " + ("vulpe" if rand else "oaie"))
 while True:
-
+    # if verifica_oi(joc.pieseOi) == True:
+        # print(True)
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             pygame.quit()
@@ -118,12 +234,13 @@ while True:
                 if distEuclid(pos, nod) <= Graph.razaPct:
                     if rand == 1:
                         piesa = piesaVulpe
-                        pieseCurente = pieseVulpi
+                        pieseCurente = joc.pieseVulpi
                     else:
                         piesa = piesaOaie
-                        pieseCurente = pieseOi
+                        pieseCurente = joc.pieseOi
+                        # print(pieseCurente)
 
-                    if nod not in pieseOi + pieseVulpi:
+                    if nod not in joc.pieseOi + joc.pieseVulpi:
 
                         if nodPiesaSelectata:
                             n0 = coordonateNoduri.index(nod)
